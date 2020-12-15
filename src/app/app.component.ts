f5bee41141cbfbd3d272bb6c95f423bc85f33fb1
sanj <sanjaykakkar088@gmail.com>
2020-12-15 11:06:28 +0530

    clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demoang';
-  result=0;
-
-  add(n1,n2){
-    console.log('n1',typeof n1);
-    console.log('n2',typeof n2);
-    this.result=Number(n1)+Number(n2);
-
-  }
-
-  multiply(n1,n2){
-    this.result=Number(n1)*Number(n2);
-  }
-
-  subtract(n1,n2){
-    this.result=Number(n1)-Number(n2);
-  }
-
-  divide(n1,n2){
-    this.result=Number(n1)/Number(n2);
-  }
 
 
 
